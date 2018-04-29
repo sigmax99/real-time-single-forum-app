d06054b203816dcaf82367542d416636b6ef35c6
Muhammad Fajrin <Muhammad Fajrin>
2018-04-29 18:58:19 +0700

    CRUD for Reply

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -6,11 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
+    protected $guarded = [];
     public function question(){
         return $this->belongsTo(Question::class);
     }
     public function user(){
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(\App\User::class);
     }
     public function like(){
         return $this->hasMany(Like::class);
